9bfaed9d5d27ab1a728d9db1ace67da560c7bbf5
Matt Munch <mattmunch94@gmail.com>
2019-09-19 20:02:16 -0700

    my madlib project

---
+++ app.js
@@ -0,0 +1,32 @@
+const adjective1 = document.getElementById ('adjective-1')
+
+
+
+
+
+function printMadlib(){
+    adjective1.textContent = 
+}
+
+
+
+
+
+
+
+
+
+
+<p id="madlib">Today I went to the zoo. I saw a <span id="adjectiveOne"></span>
+        <span id="nounOne"></span> jumping up and down in its tree. He
+        <span id="verbOne"></span>. <span id="adverbOne"></span> through
+        the large tunnel that led to its <span id="adjectiveTwo"></span>
+        <span id="nounTwo"></span>. I got some peanuts and passed them
+        through the cage to a gigantic gray <span id="nounThree"></span>
+        towering above my head. Feeding that animal made me
+        hungry. I went to get a <span id="adjectiveThree"></span> scoop of ice
+        cream. It filled my stomach. Afterwards I had to
+        <span id="verbTwo"></span> <span id="adverbTwo"></span> to catch our bus. When
+        I got home I <span id="verbThree"></span> my mom for a
+        <span id="adjectiveFour"></span> day at the zoo.</p>
+<script src="app.js"></script>
