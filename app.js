71062d0244f70af4ec0c6cff2da46b7ba982023a
Matt Munch <mattmunch94@gmail.com>
2019-09-20 09:50:31 -0700

    updated javascript file

--- app.js
+++ app.js
@@ -1,11 +1,35 @@
-const adjective1 = document.getElementById ('adjective-1')
-
-
-
-
-
-function printMadlib(){
+function printMadlib() {
+    const adjective1 = document.getElementById('adjective-1');
+    const adjective11 = adjective1.value
     adjective1.textContent = 
+    const noun1 = document.getElementById('noun-1');
+    const noun11 = noun1.value;
+    const verb1 = document.getElementById('verb-1');
+    const verb11 = verb1.value;
+    const adverb1 = document.getElementById('adverb-1');
+    const adverb11 = adverb1.value;
+    const adjective2 = document.getElementById('adjective-2');
+    const adjective22 = adjective2.value;
+    const noun2 = document.getElementById('noun-2');
+    const noun22 = noun2.value;
+    const noun3 = document.getElementById('noun-3');
+    const noun33 = noun3.value;
+    const adjective3 = document.getElementById('adjective-3');
+    const adjective33 = adjective3.value;
+    const verb2 = document.getElementById('verb-2');
+    const verb22 = verb2.value;
+    const adverb2 = document.getElementById('adverb-2');
+    const adverb22 = adverb2.value;
+    const verb3 = document.getElementById('verb-3');
+    const verb33 = verb3.value
+    const adjective4 = document.getElementById('adjective-4');
+    const adjective44 = adjective4.value
+
+
+
+
+
+
 }
 
 
@@ -18,15 +42,15 @@ function printMadlib(){
 
 
 <p id="madlib">Today I went to the zoo. I saw a <span id="adjectiveOne"></span>
-        <span id="nounOne"></span> jumping up and down in its tree. He
+    <span id="nounOne"></span> jumping up and down in its tree. He
         <span id="verbOne"></span>. <span id="adverbOne"></span> through
         the large tunnel that led to its <span id="adjectiveTwo"></span>
-        <span id="nounTwo"></span>. I got some peanuts and passed them
+    <span id="nounTwo"></span>. I got some peanuts and passed them
         through the cage to a gigantic gray <span id="nounThree"></span>
-        towering above my head. Feeding that animal made me
+    towering above my head. Feeding that animal made me
         hungry. I went to get a <span id="adjectiveThree"></span> scoop of ice
-        cream. It filled my stomach. Afterwards I had to
+    cream. It filled my stomach. Afterwards I had to
         <span id="verbTwo"></span> <span id="adverbTwo"></span> to catch our bus. When
         I got home I <span id="verbThree"></span> my mom for a
         <span id="adjectiveFour"></span> day at the zoo.</p>
-<script src="app.js"></script>
+    <script src="app.js"></script>
